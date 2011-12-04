50db00660bc0f4a0a1e042e7d9b00b90a22712a5
cjc <cjc@cjc.id.au>
2011-12-04 20:13:43 +1000

    Add public/ with less.js and bootstrap .less files. Add stub admin interface

--- index.js
+++ index.js
@@ -1,21 +1,59 @@
 var smtp = require('smtp-protocol')
-  , BufferedStream = require('morestreams').BufferedStream;
+  , BufferedStream = require('morestreams').BufferedStream
+  , express = require('express')
+  , util = require('util')
+  , dateformat = require('dateformat');
 
-var server = smtp.createServer(function (req) {
+var datalol = [];
+
+
+var smtpserver = smtp.createServer('10.2.2.89', function (req) {
     req.on('to', function (to, ack) {
-        ack.accept()
+        ack.accept(250, 'Ok')
+    });
+
+    req.on('from', function (from, ack) {
+        ack.accept(250, 'Ok')
     });
 
     req.on('message', function (stream, ack) {
         console.log(req.from + ' -> ' + req.to);
+        console.log(util.inspect(this));
+        var thismail = this;
         var buff = new BufferedStream();
         buff.on('end',function() {
-          console.log(this.chunks);
-          console.log('Size = ' + this.size);
+          //console.log(this.chunks);
+          //console.log('Size = ' + this.size);
+          console.log(util.inspect(this));
+          var mail = {};
+          mail.time = new Date() - 0;
+          mail.content = this.chunks;
+          mail.to = thismail.to;
+          mail.from = thismail.from;
+          mail.size = this.size;
+          datalol.push(mail);
         });
         stream.pipe(buff);
         ack.accept();
     });
 });
 
-server.listen(1025);
+smtpserver.listen(1025);
+
+var routes = require('./routes');
+
+var app = express.createServer();
+app.configure(function(){
+  app.use(express.static(__dirname + '/public'));
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'ejs');
+});
+
+app.get('/', routes.index);
+app.get('/mails', function(req,res) {
+  res.render('mails', { title: 'Stubtailor', data: datalol  })
+});
+
+
+app.listen(3000);
+
